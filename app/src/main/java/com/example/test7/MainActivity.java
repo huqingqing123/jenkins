368eb131b939a6d874d54769c74f0b381e410652
huqingqing <huqingqing@xiaomi.com>
2023-10-11 20:47:31 +0800

    test

---
+++ app/src/main/java/com/example/test7/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.test7;
+
+import static com.example.test7.R.layout.activity_main;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(activity_main);
+        
+    }
+}
